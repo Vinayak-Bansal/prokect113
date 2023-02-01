990dea954e24f560aa2918eafc11fc9b06b8d143
Vinayak-Bansal <97925994+Vinayak-Bansal@users.noreply.github.com>
2023-02-01 17:15:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(800, 800);
+    
+  }
+  
+  function draw() {
+    background(220);
+  
+    rect(30, 20, 55, 55);
+    rect(140, 140, 140, 60);
+    triangle(70,50,240,50,480,229)
+    let c = color(255, 204, 0);
+  fill(c);
+  noStroke();
+    ellipse(56, 300, 100, 100);
+  }
